2e9efd06ed038288832dcfd447e3ecdef1a43655
nayeong lee <lny021102@gmail.com>
2024-07-09 05:50:24 +0900

    [ADD] 네비바 디자인 수정

--- yieldme/src/Components/footer/Navi.jsx
+++ yieldme/src/Components/footer/Navi.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import StyledImg from '../Container/StyledImg';
+import CustomColumn from '../Container/CustomColumn';
+import CustomFont from '../Container/CustomFont';
 
 const NaviContainer = styled.footer`
   display: flex;
@@ -8,7 +11,7 @@ const NaviContainer = styled.footer`
   align-items: center;
   width: 100%;
   height: 60px;
-  background: #e0e0e0;
+  background: white;
   position: fixed;
   bottom: 0;
   left: 0;
@@ -21,6 +24,7 @@ const NaviButton = styled.button`
   border: none;
   font-size: 1rem;
   cursor: pointer;
+  width: 33%;
 `;
 
 const Navi = () => {
@@ -28,9 +32,24 @@ const Navi = () => {
 
   return (
     <NaviContainer>
-      <NaviButton onClick={() => navigate('/')}>홈</NaviButton>
-      <NaviButton onClick={() => navigate('/camera')}>카메라</NaviButton>
-      <NaviButton onClick={() => navigate('/mypage')}>마이</NaviButton>
+      <NaviButton onClick={() => navigate('/')}>
+        <CustomColumn width='100%' alignItems='center' justifyContent='center' gap='0.1rem'>
+          <StyledImg src={'icon_home.png'} width='2rem' height='2rem' />
+          <CustomFont color='black' font='0.7rem' fontWeight='bold'>홈</CustomFont>
+        </CustomColumn>
+      </NaviButton>
+      <NaviButton onClick={() => navigate('/camera')}>
+        <CustomColumn width='100%' alignItems='center' justifyContent='center' gap='0.1rem'>
+          <StyledImg src={'icon_scanner.png'} width='2rem' height='2rem' />
+          <CustomFont color='black' font='0.7rem' fontWeight='bold'>스캐너</CustomFont>
+        </CustomColumn>
+      </NaviButton>
+      <NaviButton onClick={() => navigate('/mypage')}>
+        <CustomColumn width='100%' alignItems='center' justifyContent='center' gap='0.1rem'>
+          <StyledImg src={'icon_my.png'} width='1.7rem' height='1.7rem' />
+          <CustomFont color='black' font='0.7rem' fontWeight='bold'>마이</CustomFont>
+        </CustomColumn>
+      </NaviButton>
     </NaviContainer>
   );
 };
