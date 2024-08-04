2a93c4d99353902146a01ca92d6a3d5f0a6c8570
nayeong lee <lny021102@gmail.com>
2024-08-05 06:32:33 +0900

    [ADD] 회원가입 API 연동 성공

--- yieldme/src/Components/header/signup/signup.jsx
+++ yieldme/src/Components/header/signup/signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import CustomFont from '../../Container/CustomFont';
 import CustomColumn from '../../Container/CustomColumn';
 import CustomRow from '../../Container/CustomRow';
@@ -65,11 +66,25 @@ const Button2 = styled.button`
 `;
 
 const GoSignup = styled.button`
-background-color: transparent;
-border: none;
-display: flex;
-align-items: center;
-justofy-content: center;
+  background-color: transparent;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+  text-align: center;
+  display: ${props => (props.show ? 'block' : 'none')};
 `;
 
 const SignUpPage = () => {
@@ -78,19 +93,54 @@ const SignUpPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [password2, setPassword2] = useState('');
+  const [modalMessage, setModalMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const isFormValid = username !== '' && password !== '' && name !== '' && password2 !== '';
 
-  const handleSignup = (e) => {
-    alert('회원가입이 완료되었습니다!');
-    navigate('/');
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      username: name,
+      login_id: username,
+      password: password,
+      password_check: password2,
+    };
+
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_SERVER}/signup/`, data);
+      console.log(response.data);
+
+      if (response && response.status === 200) {
+        setModalMessage('회원가입이 완료되었습니다!');
+        setShowModal(true);
+        setTimeout(() => {
+          setShowModal(false);
+          navigate('/');
+        }, 2000);
+      } else {
+        setModalMessage('회원가입에 실패하였습니다.');
+        setShowModal(true);
+        setTimeout(() => {
+          setShowModal(false);
+        }, 2000);
+      }
+    } catch (error) {
+      console.error(error.response ? error.response.data : error.message);
+      setModalMessage('회원가입에 실패하였습니다.');
+      setShowModal(true);
+      setTimeout(() => {
+        setShowModal(false);
+      }, 2000);
+    }
   };
+
 
   return (
     <ContainerCenter>
       <PageContainer>
         <CustomColumn width='90%' alignItems='center' justifyContent='center' gap='5rem'>
-
           <CustomColumn width='100%' alignItems='center' justifyContent='center' gap='0.5rem'>
             <StyledImg src={'logo.png'} width='130px' height='130px' />
             <CustomFont color='#FFD15B' font='1rem' fontWeight='bold'>Yello , Yield 路!</CustomFont>
@@ -127,10 +177,10 @@ const SignUpPage = () => {
                 onChange={(e) => setPassword(e.target.value)}
               />
               <Input
-                type="password2"
+                type="password"
                 placeholder="비밀번호 재입력"
                 value={password2}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setPassword2(e.target.value)}
               />
             </CustomColumn>
 
@@ -138,9 +188,12 @@ const SignUpPage = () => {
               <CustomFont color='white' font='1.1rem' fontWeight='bold'>회원가입 하기</CustomFont>
             </Button>
           </CustomColumn>
-
         </CustomColumn>
       </PageContainer>
+
+      <Modal show={showModal}>
+        <CustomFont color='black' font='1.2rem' fontWeight='bold'>{modalMessage}</CustomFont>
+      </Modal>
     </ContainerCenter>
   );
 };
