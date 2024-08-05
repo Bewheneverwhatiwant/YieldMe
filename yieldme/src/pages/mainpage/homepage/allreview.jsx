a44cc3e8f4612f2e4d5a8cd8fbf96c22ec2f16b8
nayeong lee <lny021102@gmail.com>
2024-08-05 20:36:23 +0900

    [ADD] 실제 리뷰 목록 api 연동 성공(carousel 말고 전체화면

--- yieldme/src/pages/mainpage/homepage/allreview.jsx
+++ yieldme/src/pages/mainpage/homepage/allreview.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import CustomFont from '../../../Components/Container/CustomFont';
+import { AuthContext } from '../../subpage/AuthContext';
 
 const ContainerCenter = styled.div`
   display: flex;
@@ -27,7 +29,7 @@ const PageContainer = styled(ContainerCenter)`
 const ReviewContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
- gap: 10px;
+  gap: 10px;
   justify-items: center;
   width: 100%;
 `;
@@ -36,7 +38,7 @@ const ReviewCard = styled.div`
   background-color: #FEE187;
   padding: 20px;
   border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
   width: 90%;
 `;
 
@@ -46,73 +48,31 @@ const ReviewTitle = styled.h2`
   margin-bottom: 10px;
 `;
 
-const ReviewDate = styled.p`
-  font-size: 0.8rem;
-  color: black;
-  margin-bottom: 10px;
-`;
-
 const ReviewText = styled.p`
   font-size: 0.8rem;
   color: black;
 `;
 
-const reviews = [
-    {
-        id: 1,
-        user: 'hihi0818',
-        date: '2024년 7월 14일 14시 30분에 양보받았어요.',
-        text: '임산부 때문에 너무 힘들었는데, 양보해주시고 레몬사탕까지 주셔서 너무 감사했습니다...! 좋은 일만 가득하세요...ㅠㅠ'
-    },
-    {
-        id: 2,
-        user: 'hungry4595',
-        date: '2024년 7월 15일 18시 02분에 양보받았어요.',
-        text: '오늘 몸살이 너무 심해서 힘들었는데, 퇴근길 지옥철인데도 양보해주셔서 너무 감사했어요.'
-    },
-    {
-        id: 3,
-        user: 'hihi0818',
-        date: '2024년 7월 14일 14시 30분에 양보받았어요.',
-        text: '임산부 때문에 너무 힘들었는데, 양보해주시고 레몬사탕까지 주셔서 너무 감사했습니다...! 좋은 일만 가득하세요...ㅠㅠ'
-    },
-    {
-        id: 4,
-        user: 'hungry4595',
-        date: '2024년 7월 15일 18시 02분에 양보받았어요.',
-        text: '오늘 몸살이 너무 심해서 힘들었는데, 퇴근길 지옥철인데도 양보해주셔서 너무 감사했어요.'
-    },
-    {
-        id: 5,
-        user: 'hihi0818',
-        date: '2024년 7월 14일 14시 30분에 양보받았어요.',
-        text: '임산부 때문에 너무 힘들었는데, 양보해주시고 레몬사탕까지 주셔서 너무 감사했습니다...! 좋은 일만 가득하세요...ㅠㅠ'
-    },
-    {
-        id: 6,
-        user: 'hungry4595',
-        date: '2024년 7월 15일 18시 02분에 양보받았어요.',
-        text: '오늘 몸살이 너무 심해서 힘들었는데, 퇴근길 지옥철인데도 양보해주셔서 너무 감사했어요.'
-    },
-    {
-        id: 7,
-        user: 'hihi0818',
-        date: '2024년 7월 14일 14시 30분에 양보받았어요.',
-        text: '임산부 때문에 너무 힘들었는데, 양보해주시고 레몬사탕까지 주셔서 너무 감사했습니다...! 좋은 일만 가득하세요...ㅠㅠ'
-    },
-    {
-        id: 8,
-        user: 'hungry4595',
-        date: '2024년 7월 15일 18시 02분에 양보받았어요.',
-        text: '오늘 몸살이 너무 심해서 힘들었는데, 퇴근길 지옥철인데도 양보해주셔서 너무 감사했어요.'
-    },
-];
-
 const AllReview = () => {
+    const { auth } = useContext(AuthContext);
+    const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, []);
+        const fetchReviews = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_SERVER}/all_review/`, {
+                    // headers: {
+                    //     Authorization: `Bearer ${auth.accessToken}`
+                    // }
+                });
+                setReviews(response.data);
+            } catch (error) {
+                console.error('Error fetching reviews:', error);
+            }
+        };
+        fetchReviews();
+    }, [auth.accessToken]);
 
     return (
         <ContainerCenter>
@@ -120,10 +80,9 @@ const AllReview = () => {
                 <CustomFont color='black' fontWeight='bold' font='1rem'>따뜻한 양보 후기를 전해드립니다.</CustomFont>
                 <ReviewContainer>
                     {reviews.map((review) => (
-                        <ReviewCard key={review.id}>
-                            <ReviewTitle>{review.user} 님</ReviewTitle>
-                            <ReviewDate>{review.date}</ReviewDate>
-                            <ReviewText>{review.text}</ReviewText>
+                        <ReviewCard key={review.match_id}>
+                            <ReviewTitle>{review.receiving_user__login_id} 님</ReviewTitle>
+                            <ReviewText>{review.review}</ReviewText>
                         </ReviewCard>
                     ))}
                 </ReviewContainer>
