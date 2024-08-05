186b550a1d48dcdc7387229eb3e815ef726c6862
nayeong lee <lny021102@gmail.com>
2024-08-05 22:17:23 +0900

    [ADD] 광고시청에 로그인 조건 추가

--- yieldme/src/pages/mainpage/favor/favorComponent/favor_component_4.jsx
+++ yieldme/src/pages/mainpage/favor/favorComponent/favor_component_4.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import CustomFont from '../../../../Components/Container/CustomFont';
 import CustomColumn from '../../../../Components/Container/CustomColumn';
 import CustomRow from '../../../../Components/Container/CustomRow';
 import StyledImg from '../../../../Components/Container/StyledImg';
+import { AuthContext } from '../../../subpage/AuthContext';
 
 const BannerContainer = styled.div`
   display: flex;
@@ -16,11 +17,56 @@ const BannerContainer = styled.div`
   box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1), 0 2px 10px rgba(0, 0, 0, 0.1);
 `;
 
+const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+  text-align: center;
+`;
+
+const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+`;
+
+const Button = styled.button`
+  background-color: #FEE187;
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: bold;
+  margin: 5px;
+`;
+
 const FavorComponent4 = () => {
+    const { auth } = useContext(AuthContext);
+    const [showModal, setShowModal] = useState(false);
     const navigate = useNavigate();
 
     const handleBannerClick = () => {
-        navigate('/adpage');
+        if (auth && auth.accessToken) {
+            navigate('/adpage');
+        } else {
+            setShowModal(true);
+        }
+    };
+
+    const handleCloseModal = () => {
+        navigate('/login');
+        setShowModal(false);
     };
 
     return (
@@ -37,6 +83,15 @@ const FavorComponent4 = () => {
             <CustomRow width='90%' alignItems='center' justifyContent='end'>
                 <CustomFont color='black' font='0.7rem'>클릭하여 점수 충전</CustomFont>
             </CustomRow>
+            {showModal && (
+                <>
+                    <Backdrop onClick={handleCloseModal} />
+                    <Modal>
+                        <CustomFont color='black' fontWeight='bold'>로그인 후 이용하실 수 있는 기능입니다!</CustomFont>
+                        <Button onClick={handleCloseModal}>확인</Button>
+                    </Modal>
+                </>
+            )}
         </CustomColumn>
     );
 };
