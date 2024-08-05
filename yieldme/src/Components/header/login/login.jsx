0984a9be160ff07f62298ba5cce2a4aa4d7573f6
nayeong lee <lny021102@gmail.com>
2024-08-06 05:23:25 +0900

    시도 중

--- yieldme/src/Components/header/login/login.jsx
+++ yieldme/src/Components/header/login/login.jsx
@@ -12,18 +12,20 @@ const ContainerCenter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  gap: 10px;
+  width: 100%;
   min-height: 100vh;
-  background-color: white;
 `;
 
 const PageContainer = styled(ContainerCenter)`
   width: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  padding: 8vh 0 5vh;
-  gap: 2rem;
+  justify-content: flex-start;
+  padding-top: 12vh;
+  padding-bottom: 5vh;
+  gap: 3rem;
+  position: relative;
   background-color: white;
   padding-bottom: 10vh;
 `;
